bba917e813028429a08d5c4724acce6667fc5f7e
Stefan Kerschbaumer <kerschbaumer@lean-coders.at>
2020-05-02 17:13:41 +0200

    adds package scripts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Vec3, Vec2 } from './math/extendedMath.service';
+
+export { BaseService } from './base.service';
+export { offsets } from './game/offsets';
+export { EMemoryTypes } from './process/process.interfaces';
